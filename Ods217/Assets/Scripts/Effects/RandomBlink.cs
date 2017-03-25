cd4782417760a8fc594ee370e4de5c3b1a4e4479
DESKTOP-0INOFLE\Alex <alex.woodward96@gmail.com>
2017-03-25 14:13:54 -0400

    Fixing Lighting Bugs

---
+++ Ods217/Assets/Scripts/Effects/RandomBlink.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class RandomBlink : MonoBehaviour {
+
+    [Range(.1f, 5f)]
+    public float TimerMin;
+    [Range(.1f, 10f)]
+    public float TimerMax;
+
+    float randomTime;
+    float randomBlink;
+    float currentTime;
+    Light myLight;
+
+    // Use this for initialization
+    void Start () {
+        currentTime = 0;
+        randomTime = Random.Range(TimerMin, TimerMax);
+        randomBlink = Random.Range(0, .2f);
+        myLight = GetComponent<Light>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+
+        if(currentTime > randomTime)
+        {
+            myLight.enabled = !myLight.enabled;
+
+            if (currentTime > randomTime + randomBlink)
+            {
+                currentTime = 0;
+                randomTime = Random.Range(TimerMin, TimerMax); 
+            }
+        
+
+        }
+	}
+}
---
+++ Ods217/Assets/Scripts/NonUnit/BrokenDoorScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BrokenDoorScript : MonoBehaviour, INonUnit {
+
+    public int Health;
+    public GameObject Top;
+    Animator myAnimator;
+    Animator topAnimator;
+    BoxCollider myCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        myCollider = GetComponent<BoxCollider>();
+        myAnimator = GetComponent<Animator>();
+        topAnimator = Top.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            myAnimator.SetBool("Open", true);
+            topAnimator.SetBool("Open", true);
+            myCollider.center = new Vector3(-1.6f, 0, 0.5f);
+            myCollider.size = new Vector3(0.8f, 4, 1);
+        } 
+    }
+
+
+    public bool Powered
+    {
+        get
+        { return false; }
+
+        set { }
+    }
+
+    public void OnEMP()
+    {
+        
+    }
+
+    public void OnHit()
+    {
+        Health--;
+    }
+
+}
