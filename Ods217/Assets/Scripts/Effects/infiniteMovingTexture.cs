2847e5f1aadd1d341b131f58646f700df9018cf9
DESKTOP-0INOFLE\Alex <alex.woodward96@gmail.com>
2017-09-07 16:55:46 -0400

    Content Push: Level 0 content tilesets + grubBub other animations

---
+++ Ods217/Assets/Scripts/Effects/infiniteMovingTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An Effect Script
+/// This script should be placed on two backgrounds that move continuously in the background of a scene
+/// The object will move from OriginPoint to DestinationPoint and once reaching it, will jump back to OriginPoint
+/// </summary>
+
+public class infiniteMovingTexture : MonoBehaviour {
+
+    public Vector3 OriginPoint;
+    public Vector3 DestinationPoint;
+    public float Speed;
+
+	void FixedUpdate () {
+        // Move the object
+        Vector3 direction = DestinationPoint - OriginPoint;
+        direction = direction.normalized;
+        transform.position += (direction * Speed);
+
+        // Check to see if we can go back yet
+        if((DestinationPoint - transform.position).magnitude < Speed)
+        {
+            transform.position = OriginPoint;
+        }
+
+	}
+}
