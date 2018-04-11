bf0636573abbc57d1f02e4179606bf94858d8b12
AWoodward <akw7657@g.rit.edu>
2018-04-11 17:21:31 -0400

    4/11 playtest push. Fixed thrown chairs. Fixed talking with enemies being able to move.

--- Ods217/Assets/Scripts/Unit/EnemyT1/mobTCowardStatic.cs
+++ Ods217/Assets/Scripts/Unit/EnemyT1/mobTCowardStatic.cs
@@ -39,7 +39,8 @@ public class mobTCowardStatic : mobCowardUnit
 
     void TalkDelegate()
     {
-        CutsceneManager.instance.StartCutscene(myDialog);
+        if(!CutsceneManager.InCutscene)
+            CutsceneManager.instance.StartCutscene(myDialog);
     }
 
     public override bool CheckAggro()
